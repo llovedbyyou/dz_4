383323d51c9042cd3f3035346b9047558bc9de0a
llovedbyyou <varya.balya@yandex.ru>
2024-03-10 11:32:39 +0300

    Completed

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+        long inputGold = 200L;
+        byte inputSilver = 39;
+        short inputWood = 2005;
+        long inputHealth = 97L;
+        boolean inputHasHelper = true;
+
+        Resources characterResources = new Resources(
+                inputGold,
+                inputSilver,
+                inputWood,
+                inputHealth,
+                inputHasHelper
+        );
+
+        int characterGold = (int) characterResources.gold;
+        int characterSilver = characterResources.silver;
+        double characterWood = characterResources.wood;
+        byte characterHealth = (byte) characterResources.health;
+        byte characterHelpersNumber;
+
+        // Установка значения characterHelpersNumber в зависимости от значения hasHelper
+        if (characterResources.hasHelper == true) {
+            characterHelpersNumber = 1;
+        } else {
+            characterHelpersNumber = 0;
+        }
+
+        Character character = new Character(
+                characterGold,
+                characterSilver,
+                characterWood,
+                characterHealth,
+                characterHelpersNumber
+        );
+
+        System.out.println("Персонаж создан успешно!");
+        System.out.println("Количество золота: " + character.gold);
+        System.out.println("Количество серебра: " + character.silver);
+        System.out.println("Количество дерева: " + character.wood);
+        System.out.println("Здоровье: " + character.health);
+        System.out.println("Количество помощников: " + character.helpersNumber);
+        System.out.println("Навстречу приключениям!");
+    }
+}
